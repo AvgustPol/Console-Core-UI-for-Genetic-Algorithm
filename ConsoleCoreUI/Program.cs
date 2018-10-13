b52409f807bba7fe7ae6ddfd8144af4fe26624a3
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2018-10-13 20:58:14 +0200

    Initial commit

---
+++ ConsoleCoreUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Loader;
+
+namespace ConsoleCoreUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            FileLoader loader = new FileLoader();
+
+            var res = loader.GetDataFromFileAsync().Result;
+        }
+    }
+}
