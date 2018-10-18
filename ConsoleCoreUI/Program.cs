df8994870e626dd6dcfe90228380c9ba7770ce28
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2018-10-18 14:38:13 +0200

    Added working PMX operator

--- ConsoleCoreUI/Program.cs
+++ ConsoleCoreUI/Program.cs
@@ -1,5 +1,4 @@
 ﻿using GeneticAlgorithm;
-using Loader;
 
 namespace ConsoleCoreUI
 {
@@ -7,12 +6,27 @@ namespace ConsoleCoreUI
     {
         private static void Main(string[] args)
         {
-            DataLoader loader = new DataLoader();
+            //DataLoader loader = new DataLoader();
 
-            var res = loader.GetCreatedDataContainerFromFileAsync().Result;
+            //var res = loader.GetCreatedDataContainerFromFileAsync().Result;
 
             MyGeneticAlgorithm geneticAlgorithm = new MyGeneticAlgorithm();
             geneticAlgorithm.StartGeneticAlgorithm();
+
+            #region PMX operator test
+
+            //Individual individual1 = new Individual()
+            //{
+            //    PermutationPlaces = new int[] { 8, 4, 7, 3, 6, 2, 5, 1, 9, 0 }
+            //};
+            //Individual individual2 = new Individual()
+            //{
+            //    PermutationPlaces = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            //};
+
+            //individual1.PMXoperator(individual2);
+
+            #endregion PMX operator test
         }
     }
 }
